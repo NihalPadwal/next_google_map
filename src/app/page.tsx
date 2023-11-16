94136e9f3771598b5fa17e8cd8c8ef4987ef26d6
Nihal Padwal <nihal.padwal@ting.in>
2023-11-16 15:14:38 +0530

    used revelations map api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@
 import Image from "next/image";
 // import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import GoogleMapReact from "google-map-react";
+import { useEffect, useState } from "react";
 
-const AnyReactComponent = ({
-  text,
-  lat,
-  lng,
-}: {
-  text: String;
-  lat: number;
-  lng: number;
-}) => <div>{text}</div>;
+// const AnyReactComponent = ({
+//   text,
+//   lat,
+//   lng,
+// }: {
+//   text: String;
+//   lat: number;
+//   lng: number;
+// }) => <div>{text}</div>;
 
 export default function Home() {
   // const { isLoaded } = useJsApiLoader({
@@ -22,13 +23,71 @@ export default function Home() {
 
   // console.log(isLoaded);
 
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
+  // const defaultProps = {
+  //   center: {
+  //     lat: 10.99835602,
+  //     lng: 77.01502627,
+  //   },
+  //   zoom: 11,
+  // };
+
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+
+  function initMap() {
+    const directionsService = new window.google.maps.DirectionsService();
+    const directionsRenderer = new window.google.maps.DirectionsRenderer();
+
+    const mapInstance = new window.google.maps.Map(
+      document.getElementById("map")!,
+      {
+        zoom: 10,
+        center: { lat: 37.7749, lng: -122.4194 }, // San Francisco coordinates
+      }
+    );
+
+    setMap(mapInstance);
+
+    // Set up the DirectionsRenderer to display the route
+    directionsRenderer.setMap(mapInstance);
+
+    // Set the origin and destination for the directions request
+    const origin = new window.google.maps.LatLng(37.7749, -122.4194); // San Francisco
+    const destination = new window.google.maps.LatLng(34.0522, -118.2437); // Los Angeles
+
+    const request: google.maps.DirectionsRequest = {
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+
+    // Get the directions and display them on the map
+    directionsService.route(request, (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsRenderer.setDirections(result);
+      } else {
+        console.error(`Error fetching directions: ${status}`);
+      }
+    });
+  }
+
+  useEffect(() => {
+    // Load the Google Maps API
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_MAPS_API_TING}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    // Initialize the map once the API is loaded
+    (window as any).initMap = initMap;
+
+    // Cleanup function
+    return () => {
+      if (map) {
+        window.google.maps.event.clearListeners(map, "zoom_changed");
+      }
+    };
+  }, []);
 
   return (
     <main className="min-h-screen ">
@@ -55,13 +114,14 @@ export default function Home() {
         </div>
       )} */}
 
-      <GoogleMapReact
+      {/* <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_MAPS_API_TING || "" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-      </GoogleMapReact>
+      </GoogleMapReact> */}
+      <div id="map" style={{ height: "500px", width: "100%" }} />
     </main>
   );
 }
